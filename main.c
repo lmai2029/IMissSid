58976da5f8faa09c4f96409d312fe809b844e379
lmai2029 <144735490+lmai2029@users.noreply.github.com>
2025-10-11 18:32:52 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
+/**
+ * ******************************************************************************
+ * @file    : main.c
+ * @brief   : Lab 5 Part 2 main file.
+ * 
+ * @author Leo Mai, Job Sava
+ * @date 9/26/2025
+ * ******************************************************************************
+*/
 #include "ti/devices/msp/msp.h"
-#include "uart.h"
+#include "..\Lab05\uart.h"
 #include "uart_extras.h"
-#include "leds.h"
+#include "..\Lab05\leds.h"
 #include "sysctl.h"
 #include "sysctl.c"
 #include "switches.h"
@@ -17,18 +26,18 @@ void TIMG6_IRQHandler(void);
 
 int main(void){
 	UART0_init();
-	TIMG6_init(2,255);
+	TIMG6_init(2,256);
 	ADC0_init();
-	while(1);
+	while(1){__WFI();}
 }
 
 void TIMG6_IRQHandler(void){
 	adc_value = ADC0_getVal();
-	UART0_put((uint8_t *)"Beginning conversion\n");
+	UART0_put((uint8_t *)"Beginning conversion\r\n");
 	UART0_put((uint8_t *)"Decimal value: ");
 	UART0_printUDec(adc_value);
 	UART0_put((uint8_t *)"Hexdecimal value: 0x");
 	UART0_printHex(adc_value);
+	UART0_put((uint8_t *)"\r\n");
 	TIMG6-> CPU_INT.ICLR = GPTIMER_GEN_EVENT1_ICLR_Z_CLR;
-	
 }
