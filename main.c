5804726ffeec227a060129d900744183dfa5037b
lmai2029 <144735490+lmai2029@users.noreply.github.com>
2025-10-11 18:33:19 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "ti/devices/msp/msp.h"
-#include "Lab5/camera.h"
-#include "Lab5/uart.h"
+#include "camera.h"
+#include "..\Lab05\uart.h"
 #include "sysctl.h"
 #include "sysctl.c"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "uart_extras.h"
 
 static uint16_t* cameraData;
 
@@ -14,14 +15,21 @@ int main(void){
 	Camera_init();
 	while(1){
 		if(Camera_isDataReady()){
-			UART0_put((uint8_t *)"-1\n\r");
+			UART0_put((uint8_t *)"-1\r\n");
 			cameraData = Camera_getData();
 			for(int i = 0; i < 128; i++){
-				// each data printed to PuTTY
-				UART0_put((uint8_t *)&cameraData[i]);
-				UART0_put((uint8_t *)"\n\r");
+				// each data pinted to PuTTY
+				UART0_printDec((int)cameraData[i] );
+				UART0_put((uint8_t *) "\r\n");
+				//char buffer[128];
+        //sprintf(buffer, "%u\r\n", cameraData[i]);
+        //UART0_put((uint8_t *)buffer);
+				
+				//UART0_put((uint8_t *)&cameraData[i]);
+				//UART0_put((uint8_t *)"\n\r");
 			}
-		UART0_put((uint8_t *)"-2\n\r");
+		UART0_put((uint8_t *)"-2\r\n");
 		}
 	}
 }
+
